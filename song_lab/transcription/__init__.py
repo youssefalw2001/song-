25ee5bb504184441ac1b95dfdaf410ff901fdc6a
youssefalw2001 <youssefalw2001@gmail.com>
2026-07-03 03:30:07 +0300

    Add transcription package init

---
+++ song_lab/transcription/__init__.py
@@ -0,0 +1,4 @@
+from song_lab.transcription.types import Transcript, TranscriptSegment
+from song_lab.transcription.whisper_local import LocalWhisperTranscriber
+
+__all__ = ["Transcript", "TranscriptSegment", "LocalWhisperTranscriber"]
